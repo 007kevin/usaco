fc29cfd7c8371923967fe96140adb4beb7b78c5a
Kevin <kevinkim1991@gmail.com>
2016-02-16 23:35:41 -0500

    Finished section 1.1

--- chapter-01/section-1.1/BrokenNecklace.cc
+++ chapter-01/section-1.1/BrokenNecklace.cc
@@ -4,42 +4,57 @@ LANG: C++
 TASK: beads
 Date: 15/02/2015
 Start: 7:45 pm 
-End:
+End: 11:34 pm 16/02/2015
 */
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <list>
 #define MAXN 350
 using namespace std;
+
+int beadcount(list<char>& beads){
+  int c1 = 1, c2 = 1, n = beads.size();
+  list<char>::const_iterator si, ei;
+  si = beads.begin(); ei = beads.end();
+  char color = *si++;
+  while (si != ei && c1 + c2 != n){
+    if (color == 'w') color = *si;
+    if (color != 'w' && *si != 'w' && color != *si)
+      break;
+    c1++;
+    si++;
+  }
+  list<char>::const_reverse_iterator ri, re;
+  ri = beads.rbegin(); re = beads.rend();
+  color = *ri++;
+  while (ri != re && c1 + c2 != n){
+    if (color == 'w') color = *ri;
+    if (color != 'w' && *ri != 'w' && color != *ri)
+      break;
+    c2++;
+    ri++;
+  }
+  return c1 + c2;
+}
 
 int main(){
   FILE *fin = fopen("beads.in", "r");
   FILE *fout = fopen("beads.out", "w");
-  fout = stdout;
-  int n, s, c1, counter, max;
-  counter = max = 1;
-  c1 = 0;
-  char beads[MAXN];
-  fscanf(fin, "%d\n%s", &n, beads);
-  // Set starting index the first bead of consecutive colors
-  for (s = 0; s < n - 1; s++){
-    if ((beads[s] == 'r' && beads[s + 1] == 'b') ||
-        (beads[s] == 'b' && beads[s + 1] == 'r'))
-      break;
+  int n, counter = 1, max = 2;
+  char str[MAXN];
+  list<char> beads;
+  fscanf(fin, "%d\n%s", &n, str);
+  int i = 0;
+  while (str[i]) beads.push_back(str[i++]);
+  for (i = 0; i < n; ++i){
+    int count = beadcount(beads);
+    max = count > max ? count : max;
+    beads.push_back(beads.front());
+    beads.pop_front();
   }
-  int loop = 0;
-  for (int i = (s + 1)%n; loop <= n; ++i && loop++){
-    if ((beads[i%n] == 'r' && beads[(i+1)%n] == 'b') ||
-        (beads[i%n] == 'b' && beads[(i+1)%n] == 'r') ||
-        (loop == n)){
-      max = c1 + counter > max ? c1 + counter : max;
-      c1 = counter;
-      counter = 1;
-    }
-    else
-      counter++;
-  }
-  fprintf(fout, "%d\n", max - 1);
+  
+  fprintf(fout, "%d\n", max);
   fclose(fin); fclose(fout);
   return 0;
 }
