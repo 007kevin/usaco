90c316b02a845cfe129ee75a4f88489c4a877471
Kevin <kevinkim1991@gmail.com>
2016-03-16 23:40:27 -0400

    Solved ariprog

--- chapter-01/section-1.4/ariprog2.cc
+++ chapter-01/section-1.4/ariprog2.cc
@@ -18,32 +18,31 @@ struct ans{
 };
 ans answers[10000];
 int ai;
-
 bool compare(ans a, ans b){
   return a.d < b.d;
 }
 
-void generate_seq(int a, int b, int n){
 
-  if (n == N){ // Found valid sequence
-    for (int i = 0; i < N; ++i)
-      cout << seq[i] << " ";
-    cout << endl;
+void generate_seq(int a, int b, int n){
+  
+  if (a + (b*(N-1)) > MAXBS)
+    return;
+
+  if (n == N){
+    // for (int i = 0; i < N; ++i)
+    //   cout << seq[i] << " ";
+    // cout << endl;
+    
     answers[ai].s = seq[0]; 
     answers[ai].d = b;
     ai++;
-    return;
   }
-
-  if (a + (b*(N-n)) > MAXBS)
-    return; // Valid sequence not possible
   
-  seq[n] = a + (n*b);
-  if (BS[seq[n]])
-    generate_seq(a, b, n+1);  // Continue to generate sequence
-  // else
-  //   for (int i = 1; a + (N*i) <= MAXBS; ++i)
-  //     generate_seq(a, i, 0);
+  seq[n] = a + n*b;
+  if (BS[seq[n]] && n != N)
+    generate_seq(a, b, n+1);
+  else
+    generate_seq(a, b+1, 0);
 }
 
 void solve(){
@@ -51,9 +50,11 @@ void solve(){
   for (int i = 0; i <= M; ++i)
     for (int j = 0; j <= M; ++j)
       BS[i*i + j*j] = 1;
-  
-  generate_seq(0, 1, 0);
-  
+
+  for (int i = 0; i <= MAXBS; ++i)
+    if (BS[i])
+      generate_seq(i, 1, 0);
+
   sort(answers, answers + ai, compare);
 }
 
@@ -64,13 +65,13 @@ int main(void){
 
   fin.close();
   solve();
-  
+
   if (ai == 0)
     fout << "NONE" << endl;
   else
     for (int i = 0; i < ai; i++)
       fout << answers[i].s << " " << answers[i].d << endl;
-  
+
   fout.close();
   return 0; 
 }
