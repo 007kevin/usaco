1e4f78f0629c9919fffd38d862e2536d0b4f2662
Kevin <kevinkim1991@gmail.com>
2016-03-21 08:26:47 -0400

    Failing on test case 6

---
+++ chapter-01/section-1.4/numtri.cc
@@ -0,0 +1,59 @@
+/*
+ID: min_j
+LANG: C++
+TASK: numtri
+Date: 20/03/2015
+*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+int R, maxsum;
+int ntree[1000][1000];
+int path[1000];
+int memo[1000];
+
+void solve(int lvl, int node, int csum){
+  if (lvl + 1 ==  R){
+    path[lvl] = ntree[lvl][node];
+    int sum = 0;
+    for (int i = 0; i < R; ++i){
+      //      cout << path[i] << " ";
+      sum+=path[i];
+    }
+    //    cout << sum << endl;
+    maxsum = maxsum < sum ? sum : maxsum;
+    return;
+  }
+  path[lvl] = ntree[lvl][node];
+  csum = csum + path[lvl];
+  
+  if (csum >= memo[lvl]){
+    memo[lvl] = csum;
+  
+    //    if (ntree[lvl+1][node] + memo[lvl] >= memo[lvl + 1])
+    solve(lvl + 1, node, csum);
+    //    if (ntree[lvl+1][node + 1] + memo[lvl] >= memo[lvl + 1])
+    solve(lvl + 1, node + 1, csum);
+  }
+}
+
+
+int main(){
+  ifstream fin("numtri.in");
+  ofstream fout("numtri.out");
+  fin >> R;
+  for (int i = 0 ; i < R; ++i)
+    for (int j = 0; j <= i; ++j)
+      fin >> ntree[i][j];
+  fin.close();
+  solve(0, 0, 0);
+  fout << maxsum << endl;
+
+  // for (int i = 0; i < R; ++i){
+  //   cout << "level: " << i << " " << memo[i] << endl;
+  // }
+
+  
+  fout.close();
+  return 0;
+}
