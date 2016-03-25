756fc7843e533a7423c325358e2385954181768c
Kevin <kevinkim1991@gmail.com>
2016-03-25 00:09:25 -0400

    recursively generate palindromes and test for prime

--- chapter-01/section-1.4/pprime.cc
+++ chapter-01/section-1.4/pprime.cc
@@ -8,10 +8,11 @@ Date: 21/03/2015
 #include <fstream>
 #include <string.h>
 using namespace std;
+unsigned long a, b;
 
-bool ispalindrome(int n){
+bool ispalindrome(unsigned long n){
   char num[100];
-  sprintf(num, "%d", n);
+  sprintf(num, "%lu", n);
   int i = 0;
   int j = strlen(num) - 1;
   while (i < j){
@@ -22,7 +23,63 @@ bool ispalindrome(int n){
   return true;
 }
 
+bool isprime(unsigned long n){
+  if (n == 2)
+    return true;
+  if (n == 3)
+    return true;
+  if (n % 2 == 0)
+    return false;
+  if (n % 3 == 0)
+    return false;
+
+  int i = 5;
+  int w = 2;
+  while (i * i <= n){
+    if (n % i == 0)
+      return false;
+    i+=w;
+    w = 6 - w;
+  }
+  return true;
+}
+
+int numdigits(unsigned long n){
+  char num[100];
+  sprintf(num, "%lu", n);
+  return strlen(num);
+}
+
 int main(){
-  cout << ispalindrome(100101) << endl;
+  ifstream fin("pprime.in");
+  ofstream fout("pprime.out");
+  fin>>a>>b;
+  fin.close();
+
+  unsigned long i = a;
+  if (a % 2 == 0)
+    i = a - 1;
+  for (i; i <= b && i < 10000000; i+=2){
+    int nd = numdigits(i);
+    if (i > 11 && nd%2 == 0){
+      switch(nd){
+      case 2:
+        i = 101;
+        break;
+      case 4:
+        i = 10001;
+        break;
+      case 6:
+        i = 1000001;
+        break;
+      }
+    }
+
+    if (ispalindrome(i) && isprime(i))
+      fout << i << endl;
+  }
+
+  fout.close();
+    
   return 0;
 }
---
+++ chapter-01/section-1.4/pprime1.cc
@@ -0,0 +1,97 @@
+/*
+ID: min_j
+LANG: C++
+TASK: pprime
+Date: 21/03/2015
+Comment: 
+Very fast, but very brittle.
+Recursively generate odd palindromes, but use
+the fact all even digit palindromes greater than 11 are
+divisble by 11.
+Also special edge case to print 11 after 7.
+Rushed so I can sleep.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+using namespace std;
+unsigned long a, b;
+char pal[100];
+
+FILE *fin, *fout;
+
+bool isprime(unsigned long n){
+  if (n == 2)
+    return true;
+  if (n == 3)
+    return true;
+  if (n % 2 == 0)
+    return false;
+  if (n % 3 == 0)
+    return false;
+
+  int i = 5;
+  int w = 2;
+  while (i * i <= n){
+    if (n % i == 0)
+      return false;
+    i+=w;
+    w = 6 - w;
+  }
+  return true;
+}
+
+// Note: gpal will not generate palindromes of even digits
+// but that is okay since even digit palindromes greater than 11 are
+// divisible by 11 (hence not a prime)
+void gpal(int idx, int d){
+  if (idx == d/2+1){
+    int i = idx-1;
+    while (idx != d)
+      pal[idx++] = pal[--i];
+    pal[idx] = '\0';
+    unsigned long test = atoi(pal);
+    if (a <= test && test <= b && isprime(test)){
+      fprintf(fout, "%s\n", pal);
+      if (test == 7) // special use case for 11
+        fprintf(fout, "%d\n", 11);
+    }
+    return;
+  }
+  if (idx == 0)
+    for (int i = 1; i <= 9; i+=2){
+      pal[idx] = i+'0';
+      gpal(idx+1, d);
+    }
+  else {
+    for (int i = 0; i <= 9; i++){
+      pal[idx] = i+'0';
+      gpal(idx+1, d);
+    }
+  }
+}
+
+int numdigits(unsigned long n){
+  char num[100];
+  sprintf(num, "%lu", n);
+  return strlen(num);
+}
+
+int main(){
+  fin = fopen("pprime.in", "r");
+  fout = fopen("pprime.out", "w");
+  fscanf(fin, "%lu%lu", &a, &b);
+  fclose(fin);
+
+  int ad = numdigits(a);
+  int bd = numdigits(b);
+  ad = ad%2 == 0 ? ad+1 : ad;
+  bd = bd%2 == 0 ? bd-1 : bd;
+  for (int i = ad; i <= bd; i+=2)
+    gpal(0, i);
+
+  fclose(fout);
+    
+  return 0;
+}
