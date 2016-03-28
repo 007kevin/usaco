40b5b763caf624dcbb52dbb525b4d5d31ba73e5d
Kevin <kevinkim1991@gmail.com>
2016-03-27 21:20:57 -0400

    start of ch2

---
+++ chapter-02/section-01/castle.cc
@@ -0,0 +1,17 @@
+/*
+ID: min_j
+LANG: C++
+TASK: castle
+Date: 27/03/2015
+*/
+#include <iostream>
+#include <ifstream>
+using namespace std;
+
+int main(){
+  ifstream fin("castle.in");
+  ofstream fout("castle.out");
+  
+  return 0;
+}
+
