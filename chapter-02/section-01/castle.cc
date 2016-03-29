c856ad33a036e7d84a3dc7dac9cd3f78d562359e
Kevin <kevinkim1991@gmail.com>
2016-03-29 07:49:23 -0400

    solved castle problem

--- chapter-02/section-01/castle.cc
+++ chapter-02/section-01/castle.cc
@@ -3,6 +3,38 @@ ID: min_j
 LANG: C++
 TASK: castle
 Date: 27/03/2015
+Analysis:
+It was WRONG to assume the largest room would
+alway be part of the largest capable room.
+Even though there exists the largest room, 
+a sum of two smaller rooms may be larger!!!
+
+#########################################
+#   |   |   |   #   #   |   |   |   |   #
+#################-------#####-----------#
+#   |   |   |   #   #   |   |   |   |   #
+----------------------------------------#
+#   |   |   |   #   #   |   |   |   |   #
+----------------------------------------#
+#   |   |   |   #   #   |   |   |   |   #
+----------------#####################---
+#   |   |   |   #   #   |   |   |   #   #
+--------------------##################### <--
+#   |   |   |   #   #   #   |   |   |   #
+----------------------------------------#
+#   |   |   |   #   #   #   |   |   |   #
+----------------------------------------#
+#   |   |   |   #   #   #   |   |   |   #
+----------------#####-------------------#
+#   |   |   |   #   #   #   |   |   |   #
+----------------------------------------#
+#   |   |   |   #   #   #   |   |   |   #
+
+largest room is 36, but largest capable 
+room is 41
+
+Next time, keep track of the components instead!
+
 */
 #include <iostream>
 #include <fstream>
@@ -17,9 +49,13 @@ using namespace std;
 #define south(x) ((x) & 0x8)
 int M, N, num_components;
 vector<int*> allocated;
+
 struct room {
   int size, component, n, m;
 } largest_room;
+room actual_largest_room; // hack. Can't use largest_room since its been written to code too much
+
+vector<room> rooms;
 
 struct ifroom {
   int size, n, m;
@@ -184,16 +220,30 @@ void find_components(){
         int *allocated_size = (int*) calloc(1, sizeof(int));
         allocated.push_back(allocated_size);
         flood_fill(i,j,++num_components, allocated_size);
-        if (largest_room.size < *floorplan[i][j].size){
-          largest_room.n = i;
-          largest_room.m = j;
-          largest_room.size = *floorplan[i][j].size;
-          largest_room.component = num_components;
+        room tmp;
+        tmp.size = *floorplan[i][j].size;
+        tmp.n = i;
+        tmp.m = j;
+        tmp.component = num_components;
+        rooms.push_back(tmp);
+        if (actual_largest_room.size < *floorplan[i][j].size){
+          actual_largest_room.n = i;
+          actual_largest_room.m = j;
+          actual_largest_room.size = *floorplan[i][j].size;
+          actual_largest_room.component = num_components;
         }
       }
-  find_largest_creatable(largest_room.n,
-                         largest_room.m,
-                         largest_room.component);
+  while (!rooms.empty()){
+    largest_room = rooms.back();
+    find_largest_creatable(largest_room.n,
+                           largest_room.m,
+                           largest_room.component);
+    // reset visited
+    for (int i = 0; i < N; ++i)
+      for (int j = 0; j < M; ++j)
+        floorplan[i][j].v = 0;
+    rooms.pop_back();
+  }
 }
 
 int main(){
@@ -214,32 +264,43 @@ int main(){
   // cout << largest_capable_room.w << endl;
 
   fout << num_components << endl;
-  fout << largest_room.size << endl;
+  fout << actual_largest_room.size << endl;
   fout << largest_capable_room.size << endl;
   fout << largest_capable_room.n + 1 << " ";
   fout << largest_capable_room.m + 1 << " ";
   fout << largest_capable_room.w << endl;
 
+  // MAP DEBUGGER
   // for (int i = 0; i < N; ++i){
-  //   if (i == 0){
-  //     for (int i1 = 0; i1 < N; ++i1)
-  //       cout << "- ";
-  //     cout << endl;
+  //   for (int j = 0; j < M; ++j){
+  //     if (j == 0 && north(floorplan[i][j].w))
+  //       cout << "#####";
+  //     else if (north(floorplan[i][j-1].w))
+  //       if (north(floorplan[i][j].w))
+  //         cout << "####";
+  //       else
+  //         cout << "---";
+  //     else
+  //       if (north(floorplan[i][j].w))
+  //         cout << "#####";
+  //       else
+  //         cout << "----";
+  //     if (j == M-1 && !north(floorplan[i][j-1].w))
+  //       cout << "#";
   //   }
+  //   cout << endl;
   //   for (int j = 0; j < M; ++j){
   //     if (j == 0)
-  //       cout << "|";
-  //     if (east(floorplan[i][j].w))
-  //       cout << "|"; 
+  //       cout << "#   ";
+  //     else
+  //       if (west(floorplan[i][j].w))
+  //         cout << "#   ";
+  //       else
+  //         cout << "|   ";
   //     if (j == M-1)
-  //       cout << "|";
-  //     cout << endl;
+  //       cout << "#";
   //   }
-  //   if (i == N-1){
-  //     for (int i1 = 0; i1 < N; ++i1)
-  //       cout << "- " ;
-  //     cout << endl;
-  //   }
+  //   cout << endl;
   // }
 
   // free allocated memory
