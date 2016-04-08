097004e37bc73adac88d7ae43433e8ddad19de97
Kevin <kevinkim1991@gmail.com>
2016-04-07 22:14:02 -0400

    start of section 2

---
+++ chapter-02/section-02/preface.cc
@@ -0,0 +1,19 @@
+/*
+ID: min_j
+LANG: C++
+TASK: preface
+Date: 07/04/2015
+Analysis:
+*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+  ifstream fin("preface.in");
+  ofstream fout("preface.out");
+  
+  fin.close();
+  fout.close();
+  return 0;
+}
