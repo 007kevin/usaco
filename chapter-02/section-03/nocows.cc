7a85e78bb00608942f8654428a76e8192fa75a81
kevkim <kevinkim1991@gmail.com>
2016-04-18 09:26:06 -0400

    added to analysis

--- chapter-02/section-03/nocows.cc
+++ chapter-02/section-03/nocows.cc
@@ -4,4 +4,7 @@ LANG: C++
 TASK: prefix
 Date: 17/04/2015
 Anaylsis:
+  Find the recurrence relation. Given the problem, we know
+  that nodes can have either 0 or 2 children.
+  How can we find all the binary pedigrees?
 */
