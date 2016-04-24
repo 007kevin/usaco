7bd541014eadc5e5df7f8cc33b056f9e52865dde
Kevin <kevinkim1991@gmail.com>
2016-04-24 11:03:19 -0400

    stuck on test 9

--- chapter-02/section-03/concom.cc
+++ chapter-02/section-03/concom.cc
@@ -8,9 +8,22 @@ Anaylsis:
 */
 #include <iostream>
 #include <fstream>
+#include <cstring>
 using namespace std;
 int N;
 int graph[101][101];
+
+void own(int a){
+  for (int i = 1; i <= 100; ++i){
+    if (graph[a][i] > 50 && graph[i][0] == 0){
+      graph[i][0] = 1;
+      own(i);
+      for (int j = 1; j <= 100; ++j)
+        graph[a][j] += graph[i][j];
+      i = 1;
+    }
+  }
+}
 
 int main(){
   ifstream fin("concom.in");
@@ -22,6 +35,27 @@ int main(){
     fin>>graph[a][b];
   }
   
+  // //Debug
+  // for (int i = 1; i <= 100; ++i){
+  //   for (int j = 1; j <= 100; ++j){
+  //     string space = " ";
+  //     if (graph[i][j] < 10)
+  //       space+=" ";
+  //     cout << graph[i][j] << space;
+  //   }
+  //   cout << endl;
+  // }
+  
+  for (int i = 1; i <= 100; ++i){
+    own(i);
+  }
+
+  for (int i = 1; i <= 100; ++i)
+    for (int j = 1; j <= 100; ++j){
+      if (i != j && graph[i][j] > 50)
+        fout << i << " " << j << endl;
+    }
+
 }
     
 
