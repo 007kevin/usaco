d9f74aba3307a74240f761b064e3b92e28233742
Kevin <kevinkim1991@gmail.com>
2016-05-04 07:13:12 -0400

    start of frac dec

---
+++ chapter-02/section-04/fracdec.cc
@@ -0,0 +1,22 @@
+/*
+ID: min_j
+LANG: C++
+TASK: fracdec
+Date: 04/05/2015
+Anaylsis:
+*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+int N,D;
+
+int main(){
+  ifstream fin("fracdec.in");
+  ofstream fout("fracdec.out");
+  fin>>N>>D;
+  fin.close();
+
+
+  fout.close();
+  return 0;
+}
