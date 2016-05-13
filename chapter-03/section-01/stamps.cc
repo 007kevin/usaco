2fcca4dc5bf1189037ed400df552cee842a50f49
Kevin <kevinkim1991@gmail.com>
2016-05-12 20:56:52 -0400

    solved stamps. Inital intuition was correct

--- chapter-03/section-01/stamps.cc
+++ chapter-03/section-01/stamps.cc
@@ -4,18 +4,25 @@ LANG: C++
 TASK: stamps
 Date: 11/05/2015
 Anaylsis:
-  We may need to use recursion to keep track
-  of the number of stamps
+  At each index of dp,
+  we generate the next indexes by iterating
+  through the stamp values. dp[i] is the number
+  of stamps, therefore if dp[i] is greater than K,
+  we don't generate next index.
+
+  Also, if dp[i] has already been generated from
+  previous stamps, we set the minimum of stamps used
+
+
 */
 #include <cstdio>
 #include <cassert>
 #define MAXK 200
+#define MAXS 2000000
 int stamp[MAXK+1];
-struct pos {
-  int cent,num;
-};
-pos dp[10001];
-int K,N;
+int dp[MAXS+1];
+int K,N,I;
+
 int main(){
   FILE *fin = fopen("stamps.in","r");
   FILE *fout = fopen("stamps.out","w");
@@ -24,18 +31,23 @@ int main(){
   for (int i = 0; i < N; ++i)
     fscanf(fin,"%d",stamp+i);
   fclose(fin);
+
   int i;
-  for (i = 0; i <= 10000; ++i)
-    if (i != 0 && dp[i].cent == 0)
+  for (i = 0; i <= MAXS; ++i){
+    if (i != 0 && dp[i] == 0)
       break;
-    else
-      for (int j = 0; j < N; ++j)
-        if (i+stamp[j] <= 10000 && dp[i].num <= K ){
-          dp[i+stamp[j]].cent+=stamp[j];
-          dp[i+stamp[j]].num=dp[i].num+1;
-        }
-  for (int i = 0; i < 10000; ++i)
-    printf("%d %d\n",dp[i].cent,dp[i].num);
+    if (i == 0 || dp[i] < K)
+      for (int j = 0; j < N; ++j){
+        if (dp[i+stamp[j]] != 0)
+          dp[i+stamp[j]] = dp[i+stamp[j]] > dp[i] + 1 ? dp[i]+1 : dp[i+stamp[j]];
+        else
+          dp[i+stamp[j]] = dp[i]+1;
+      }
+  }
   fprintf(fout,"%d\n",i-1);
+
+  // for (int i = 0; i < 100; ++i)
+  //   printf("%d: %d\n",i,dp[i]);
+  
   fclose(fout);
 }
