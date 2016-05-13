723697126246bd1b4dbf8cdf4a6da06998f6abd7
Kevin <kevinkim1991@gmail.com>
2016-05-13 19:24:54 -0400

    solved fact4

--- chapter-03/section-02/fact4.cc
+++ chapter-03/section-02/fact4.cc
@@ -4,6 +4,11 @@ LANG: C++
 TASK: fact4
 Date: 13/05/2015
 Anaylsis:
+  Since max N is 4220, we can keep modding by 10000 as we build up the N
+  factorial.
+  The solution insight though was to eliminate 2's and 5's in the factorial's
+  product so that values don't end in 10. For every number of 2's that were avoided, 
+  the same amount of 5 must de done
 */
 #include <cstdio>
 #include <cassert>
@@ -21,13 +26,13 @@ int main(){
     long next = dp[i-1]*i;
     while (next%10 == 0)
       next/=10;
-    dp[i] = next%10;
+    dp[i] = next%10000;
   }
-  double cur = 1;
-  for (int i = 1; i <= N; ++i){
-    cur*=i;
-    printf("%d %d: %.0f\n",i,dp[i],cur);
-  }
-  fprintf(fout,"%d\n",dp[N]);
+  // double cur = 1;
+  // for (int i = 1; i <= N; ++i){
+  //   cur*=i;
+  //   printf("%d %d: %.0f\n",i,dp[i],cur);
+  // }
+  fprintf(fout,"%d\n",dp[N]%10);
   return 0;
 }
