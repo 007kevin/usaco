fc9aa5d48248a261e3e2d8549886ac9127554558
Kevin <kevinkim1991@gmail.com>
2016-05-25 20:30:13 -0400

    have to think about problem some more

---
+++ chapter-03/section-03/shopping.cc
@@ -0,0 +1,18 @@
+/*
+ID: min_j
+LANG: C++
+TASK: shopping
+Date: 25/05/2015
+Anaylsis:
+*/
+#include <cstdio>
+
+int main(){
+  FILE *fin = fopen("shopping.in","r");
+  FILE *fout = fopen("shopping.out","w");
+
+  fclose(fin);
+  fclose(fout);
+  return 0;
+}
+
