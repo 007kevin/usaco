5f5686d84eadc3e555bbe42531f4314e8be9f24a
Kevin <kevinkim1991@gmail.com>
2016-05-27 22:56:10 -0400

    must do dp

--- chapter-03/section-03/shopping_dp.cc
+++ chapter-03/section-03/shopping_dp.cc
@@ -4,23 +4,10 @@ LANG: C++
 TASK: shopping
 Date: 25/05/2015
 Anaylsis:
-  For each special offer, we calculated the amount saved,
-  then apply greedy algorithm against the items required.
-
-  What happens when the amount saved are the same for
-  different special offers? 
-    - Should we choose the one that optimally allows the
-      choosing of other special offers down the road?
-      - Anytime we come across special offers of the same price,
-        attempt all combinations of taking those special offers
-
-  This solution fails on test case 10.. too slow. It looks like
-  we must solve via DP
 */
 #include <cstdio>
 #include <cassert>
 #include <climits>
-#include <algorithm>
 //#define DEBUG
 #define MAXN 5
 #define MAXS 99
@@ -29,48 +16,20 @@ using namespace std;
 struct special_offer{
   int n,r,p,s; // #products, reduced priced, actual price, savings
   int c[MAXN],k[MAXN]; // prod. code, #items
-  bool operator<(const special_offer& rhs) const {
-    return s > rhs.s;
-  }
 };
 special_offer S[MAXS];
 int s,solution = INT_MAX;
 int amt[MAXC+1];
 int price[MAXC+1];
 
-void solve(int cprice, int I){
-  if (I == s){
-    for (int i = 1; i <= MAXC; ++i)
-      cprice+=amt[i]*price[i];
-    solution = cprice < solution ? cprice : solution;
-#ifdef DEBUG
-    printf("cprice: %d\n", cprice);
-#endif
-    return;
+int solve(int I){
+  int p = 0;
+  if (I == 0){
+    for (int i = 0; i < MAXC; ++i)
+      p+=amt[i]*price[i];
+    return p;
   }
-  int same = 1;
-  // while (I+same < s && S[I].s == S[I+same].s)
-  //   same++;
-  special_offer *cur;
-  for (int i = 0; i < same; ++i){
-    cur = &S[I+i];
-    int flag = 1;
-    for (int j = 0; j < cur->n; ++j)
-      if (amt[cur->c[j]] - cur->k[j] < 0){
-        flag = 0; break;
-      }
-    if (flag){
-      for (int j = 0; j < cur->n; ++j){
-        amt[cur->c[j]]-=cur->k[j];
-      }
-      solve(cprice + cur->r, I);
-      for (int j = 0; j < cur->n; ++j){
-        amt[cur->c[j]]+=cur->k[j];
-      }
-    }
-    
-  }
-  solve(cprice, I+1);
+  p = solve(I-1);
 }
   
 int main(){
@@ -103,9 +62,6 @@ int main(){
     cur->s = cur->p - cur->r;
   }
 
-  // Sort the special offers by amount saved
-  sort(S,S+s);
-  
 #ifdef DEBUG
   printf("s\tr\tp\ts\tn\tc\tk\n");
   for (int i = 0; i < s; ++i){
@@ -116,11 +72,8 @@ int main(){
              S[i].c[j],S[i].k[j]);
   }
 #endif
-
-  solve(0,0);
-  
+  solve(s);
   fprintf(fout,"%d\n",solution);
-
   fclose(fout);
   return 0;
 }
