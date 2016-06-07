60a1072a5ae30f846525f94f4ba4cdf231f4fce0
Kevin <kevinkim1991@gmail.com>
2016-06-07 07:19:22 -0400

    working on range

--- chapter-03/section-03/range.cc
+++ chapter-03/section-03/range.cc
@@ -4,6 +4,8 @@ LANG: C++
 TASK: range
 Date: 07/06/2016
 Anaylsis:
+  Find the biggest unravaged field, mark it,
+  then iterate the unvisited?
 */
 #include <iostream>
 #include <fstream>
@@ -14,7 +16,25 @@ int field[MAXN][MAXN];
 int squares[MAXN];
 
 void solve(int m){
-  int offset = 
+  if (m < 2)
+    return;
+  for (int i = 0; i+m <= N; ++i)
+    for (int j = 0; j+m <= N; ++j){
+      bool ravaged = false;
+      for (int k = i; k <= m; ++k){
+        for (int l = j; l <= m; ++l)
+          if (field[k][l] == 0){
+            ravaged = true;
+            break;
+          }
+        if (ravaged)
+          break;
+      }
+      if (!ravaged){
+        squares[m]++;
+      }
+    }
+  solve(m-1);
 }
 
 
@@ -24,8 +44,10 @@ int main(){
   fin>>N;
   for (int i = 0; i < N; ++i)
     for (int j = 0; j < N; ++j)
-      fin>>squares[i][j];
+      fin>>field[i][j];
   solve(N);
+  for (int i = 2; i <= N; ++i)
+    fout << i << " " << squares[i] << endl;
  
   
 }
