fa96a91272b7f323648f8f5f06bfdbc3c6664595
Kevin Kim <kevkim@blackberry.com>
2016-07-12 20:59:02 +0000

    solved?

--- chapter-04/section-01/fence6.cc
+++ chapter-04/section-01/fence6.cc
@@ -22,22 +22,43 @@ int visit[2][MAXN];
 int slen[MAXN];
 
 void find(int s, int c, int p){
-  if (!visit[0][c]){
-    visit[0][c] = 1;
-    for (int i = 1; i <= N; ++i){
-      if (matrix[c][1][i]){
-        visit[1][c] = 1;
-        find(s,i,p+slen[c]);
-        visit[1][c] = 0;
-      }
-    }
+  if (p > sol) return;
+  if (s == c && p != 0){
+    sol = min(sol,p);
+    cout << sol << endl;
+    return;
   }
-  else {
+  if (!visit[0][c]){
     for (int i = 1; i <= N; ++i){
       if (matrix[c][0][i]){
         visit[0][c] = 1;
+        if (matrix[i][0][c])
+          visit[0][i] = 1;
+        else
+          visit[1][i] = 1;
         find(s,i,p+slen[c]);
+        if (matrix[i][0][c])
+          visit[0][i] = 0;
+        else
+          visit[1][i] = 0;
         visit[0][c] = 0;
+      }
+    }
+  }
+  if (!visit[1][c]){
+    for (int i = 1; i <= N; ++i){
+      if (matrix[c][1][i]){
+        if (matrix[i][1][c])
+          visit[1][i] = 1;
+        else
+          visit[0][i] = 1;
+        visit[1][c] = 1;
+        find(s,i,p+slen[c]);
+        if (matrix[i][1][c])
+          visit[1][i] = 0;
+        else
+          visit[0][i] = 0;
+        visit[1][c] = 0;
       }
     }
   }
@@ -62,10 +83,14 @@ int main(){
       matrix[s_num][1][e] = 1;
     }
   }
-  find(1,1,0);
+  for (int i = 1; i <= N; ++i){
+    cout << i << " ============" << endl;
+    visit[0][i] = 1;
+    find(i,i,0);
+  }
   fin.close();
 
-  find_smallest_perimeter(1,1,0);
+  find(1,1,0);
 
   fout << sol << endl;
   fout.close();
