59db4fc8cd0e8990c0d895748f4f118419063c47
Kevin <kevinkim1991@gmail.com>
2016-07-12 08:27:40 -0400

    differentiating fence segments

--- chapter-04/section-01/fence6.cc
+++ chapter-04/section-01/fence6.cc
@@ -17,18 +17,30 @@ Anaylsis:
 #define MAXN 101
 using namespace std;
 int N, sol = INT_MAX;
-int matrix[MAXN][MAXN];
+int matrix[MAXN][2][MAXN];
+int visit[2][MAXN];
 int slen[MAXN];
 
-void find_smallest_perimeter(int n, int m, int p){
-  if (p > sol) return;
-  if (n == m && p != 0){
-    sol = min(sol,p);
-    return;
+void find(int s, int c, int p){
+  if (!visit[0][c]){
+    visit[0][c] = 1;
+    for (int i = 1; i <= N; ++i){
+      if (matrix[c][1][i]){
+        visit[1][c] = 1;
+        find(s,i,p+slen[c]);
+        visit[1][c] = 0;
+      }
+    }
   }
-  for (int i = 1; i <= N; ++i)
-    if (matrix[m][i] && m != i)
-      find_smallest_perimeter(n,i,p+slen[m]);
+  else {
+    for (int i = 1; i <= N; ++i){
+      if (matrix[c][0][i]){
+        visit[0][c] = 1;
+        find(s,i,p+slen[c]);
+        visit[0][c] = 0;
+      }
+    }
+  }
 }
 
 int main(){
@@ -43,40 +55,17 @@ int main(){
     slen[s_num] = len;
     for (int j = 0; j < N1; ++j){
       fin>>e;
-      matrix[s_num][e] = 1;
+      matrix[s_num][0][e] = 1;
     }
     for (int j = 0; j < N2; ++j){
       fin>>e;
-      matrix[s_num][e] = 1;
+      matrix[s_num][1][e] = 1;
     }
   }
+  find(1,1,0);
   fin.close();
-  
-  for (int i = 1; i <= N; ++i){
-    find_smallest_perimeter(i,i,0);
-  }
 
-  cout << "\t";
-  for (int i = 0; i <= N; ++i)
-    cout << i << " ";
-  cout << endl;
-  for (int i = 0; i <= N; ++i){
-    cout << i << "\t";
-    for (int j = 0; j <= N; ++j){
-      if (matrix[i][j] == 0)
-        cout << "  ";
-      else
-        cout << matrix[i][j] << " ";
-    }
-    cout << endl;
-  }
-
-  for (int i = 0; i <= N; ++i)
-    cout << i << "\t";
-  cout << endl;
-  for (int i = 0; i <= N; ++i)
-    cout << slen[i] << "\t";
-  cout << endl;
+  find_smallest_perimeter(1,1,0);
 
   fout << sol << endl;
   fout.close();
