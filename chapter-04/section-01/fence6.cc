6bcd56cef839c94df836669fe55d5fa5f338b66f
Kevin <kevinkim1991@gmail.com>
2016-07-14 12:54:33 -0400

    solved fence loops

--- chapter-04/section-01/fence6.cc
+++ chapter-04/section-01/fence6.cc
@@ -1,7 +1,7 @@
 /*
 ID: min_j
 LANG: C++
-TASK: fence9
+TASK: fence6
 Date: 11/07/2016
 Anaylsis:
   Build an adjacency matrix of fence endpoints
@@ -13,53 +13,56 @@ Anaylsis:
 #include <fstream>
 #include <cassert>
 #include <climits>
-#define min(a,b) ((a) < (b) ? a : b)
-#define MAXN 101
 using namespace std;
-int N, sol = INT_MAX;
-int matrix[MAXN][2][MAXN];
-int visit[2][MAXN];
-int slen[MAXN];
 
-void find(int s, int c, int p){
+#define MAXN 101
+int len[MAXN];
+int con[MAXN][MAXN][2]; // for con[i][[j][0] = 1 means segment i
+                        // is connected to segment j through endpoint 0
+                        // of i
+int visit[MAXN][2];
+int N;
+int sol;
+void find(int s,int c, int p){
   if (p > sol) return;
   if (s == c && p != 0){
-    sol = min(sol,p);
-    cout << sol << endl;
+    sol = p < sol ? p : sol;
     return;
   }
-  if (!visit[0][c]){
+  if (visit[c][0] == 0){
+    visit[c][0] = 1;
     for (int i = 1; i <= N; ++i){
-      if (matrix[c][0][i]){
-        visit[0][c] = 1;
-        if (matrix[i][0][c])
-          visit[0][i] = 1;
-        else
-          visit[1][i] = 1;
-        find(s,i,p+slen[c]);
-        if (matrix[i][0][c])
-          visit[0][i] = 0;
-        else
-          visit[1][i] = 0;
-        visit[0][c] = 0;
+      if (con[c][i][0]){
+        if (con[i][c][0])
+          visit[i][0] = 1;
+        if (con[i][c][1])
+          visit[i][1] = 1;
+        find(s,i,p+len[c]);
+        if (con[i][c][0])
+          visit[i][0] = 0;
+        if (con[i][c][1])
+          visit[i][1] = 0;
       }
     }
+    visit[c][0] = 0;
   }
-  if (!visit[1][c]){
-    for (int i = 1; i <= N; ++i){
-      if (matrix[c][1][i]){
-        if (matrix[i][1][c])
-          visit[1][i] = 1;
-        else
-          visit[0][i] = 1;
-        visit[1][c] = 1;
-        find(s,i,p+slen[c]);
-        if (matrix[i][1][c])
-          visit[1][i] = 0;
-        else
-          visit[0][i] = 0;
-        visit[1][c] = 0;
+  else {
+    if (visit[c][1] == 0){
+      visit[c][1] = 1;
+      for (int i = 1; i <= N; ++i){
+        if (con[c][i][1]){
+          if (con[i][c][0])
+            visit[i][0] = 1;
+          if (con[i][c][1])
+            visit[i][1] = 1;
+          find(s,i,p+len[c]);
+          if (con[i][c][0])
+            visit[i][0] = 0;
+          if (con[i][c][1])
+            visit[i][1] = 0;
+        }
       }
+      visit[c][1] = 0;
     }
   }
 }
@@ -69,28 +72,30 @@ int main(){
   ofstream fout("fence6.out");
   assert(fin.is_open());
   assert(fout.is_open());
-  int N1,N2,s_num,len,e;
   fin>>N;
+  int s,l,n1,n2,e;
   for (int i = 0; i < N; ++i){
-    fin>>s_num>>len>>N1>>N2;
-    slen[s_num] = len;
-    for (int j = 0; j < N1; ++j){
+    fin>>s>>l>>n1>>n2;
+    sol+=l;
+    len[s] = l;
+    for (int j = 0; j < n1; ++j){
       fin>>e;
-      matrix[s_num][0][e] = 1;
+      con[s][e][0] = 1;
     }
-    for (int j = 0; j < N2; ++j){
+    for (int j = 0; j < n2; ++j){
       fin>>e;
-      matrix[s_num][1][e] = 1;
+      con[s][e][1] = 1;
     }
-  }
-  for (int i = 1; i <= N; ++i){
-    cout << i << " ============" << endl;
-    visit[0][i] = 1;
-    find(i,i,0);
   }
   fin.close();
+  if (N < 3){
+    fout << "0" << endl;
+    return 0;
+  }
 
-  find(1,1,0);
+  for (int i = 1; i <= N; ++i){
+    find(i,i,0);
+  }
 
   fout << sol << endl;
   fout.close();
