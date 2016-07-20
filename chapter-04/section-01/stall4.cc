ea5fd6a372486f7d23d26145aff3c502e0c9d548
Kevin <kevinkim1991@gmail.com>
2016-07-19 21:02:14 -0400

    solved stall4

--- chapter-04/section-01/stall4.cc
+++ chapter-04/section-01/stall4.cc
@@ -1,9 +1,12 @@
-/*
+n/*
 ID: min_j
 LANG: C++
 TASK: stall4
 Date: 19/07/2016
 Anaylsis:
+  Note - When applying the ford-fulkerson algorithm, it is
+         important to maintain skew symmetry and also to keep 
+         track of visited vertices to prevent infinite loops
 */
 #include <iostream>
 #include <fstream>
@@ -12,14 +15,17 @@ Anaylsis:
 using namespace std;
 int N,M,sol;
 int matrix[MAX][MAX];
+int visit[MAX];
 int augment_path(int n){
+  visit[n] = 1;
   if (n == N+M+1){
     sol++;
     return 1;
   }
   for (int i = 1; i <= N+M+1; ++i)
-    if (matrix[n][i] && augment_path(i)){
-      matrix[n][i] = 0;
+    if (visit[i] == 0 && matrix[n][i] && augment_path(i)){
+      matrix[n][i]--;
+      matrix[i][n]++;      
       return 1;
     }
   return 0;
@@ -43,7 +49,10 @@ int main(){
   for (int i = N+1; i <= N+M; ++i)
     matrix[i][N+M+1] = 1;
   fin.close();
-  while(augment_path(0));
+  while(augment_path(0)){
+    for (int i = 0; i <= N+M+1; ++i)
+      visit[i] = 0;
+  }
   fout << sol << endl;
   fout.close();
   return 0;
