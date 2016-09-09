ae8ab93f22e4f35c418a54c11e1453f94172a557
kevinkim1991@gmail.com <kevinkim1991@gmail.com>
2016-09-09 06:10:23 -0400

    need to test solution against judge

---
+++ chapter-04/section-02/race3.cc
@@ -0,0 +1,97 @@
+/*
+ID: min_j
+LANG: C++
+TASK: race3
+Date: 09/09/2016
+Anaylsis:
+  There are at most 50 nodes and 100 edges therefore we can remove 
+  each node and test if path is possible. As for detecting well 
+  formed splitting points P, mark all nodes that come before P and 
+  see if there are any paths to the marked nodes.
+*/
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cassert>
+#define MAXP 50
+using namespace std;
+
+int P;
+int course[MAXP][MAXP];
+int visit[MAXP];
+int visit1[MAXP];
+vector<int> UP;
+vector<int> SP;
+// std::ostream& operator<<(std::ostream &out, const (int *C)[MAXP]){
+//   out << C[0][0] << std::endl;
+//   return out;
+// }
+
+bool unavoidable_point(int s, int p){
+  if (s == P-1)
+    return false;
+  if (visit[s] == p)
+    return true;
+  visit[s] = p;
+  for (int i = 0; i < P; ++i)
+    if (course[s][i] && i != p)
+      if (!unavoidable_point(i,p))
+        return false;
+
+  return true;
+}
+
+bool splitting_point(int s, int p){
+  if (visit[s] == p)
+    return false;
+  if (visit1[s] == p)
+    return true;
+  visit1[s] = p;
+  for (int i = 0; i < P; ++i)
+    if (course[s][i] && i != p)
+      if (!splitting_point(i,p))
+        return false;
+  return true;
+}
+
+
+int main(){
+  ifstream fin("race3.in");
+  ofstream fout("race3.out");
+  assert(fin.is_open());
+  assert(fout.is_open());
+  int p;
+  while (true){
+    fin>>p;
+    if (p == -1) break;
+    if (p == -2) P++;
+    else {
+      course[P][p] = 1;
+    }
+  }
+  
+  for (int i = 1; i < P-1; ++i){
+    if (unavoidable_point(0,i)){
+      UP.push_back(i);
+      if (splitting_point(i,i))
+        SP.push_back(i);
+    }
+  }
+
+  // DEBUG
+  // for (int i = 0; i < P; ++i){
+  //   for (int j = 0; j < P; ++j)
+  //     cout << course[i][j] << " ";
+  //   cout << endl;
+  // }
+  
+  fout << UP.size() << " ";
+  for (int i = 0; i < UP.size(); ++i)
+    fout << UP[i] << " ";
+  fout << endl;
+  fout << SP.size() << " ";
+  for (int i = 0; i < SP.size(); ++i)
+    fout << SP[i] << " ";
+  fout << endl;
+  return 0;
+}
