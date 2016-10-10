5b70c3316968cdbe840e04db4170933894f0d588
kevin <kevin>
2016-10-10 13:13:43 -0700

    done section 4.3

--- chapter-04/section-02/lgame.cc
+++ chapter-04/section-02/lgame.cc
@@ -2,180 +2,130 @@
 ID: min_j
 LANG: C++
 TASK: lgame
-Date: 09/28/2016
+Date: 10/07/2016
 Anaylsis:
 */
-#include <iostream>
-#include <fstream>
-#include <unordered_map>
-#include <algorithm>
-#include <list>
-#include <set>
-#include <cassert>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<map>
+#include<set>
+#include<vector>
+#include<algorithm>
+#include<cassert>
 using namespace std;
 
-class Pointify {
-  struct words {
-    int point;
-    list<string> L;
-    words() : point(0) {};
-    words(int p) : point(p) {}
-  };
-  unordered_map<string,words> H;
-public:
+int eval_pts(const string &str){
+  int p[] = {2,5,4,4,1,6,5,5,1,7,6,3,5,
+          // a b c d e f g h i j k l m 
+             2,3,5,7,2,1,2,4,6,6,7,5,7};
+          // n o p q r s t u v w x y z
+  int pt = 0;
+  for (int i = 0; i < str.size(); ++i)
+    pt+=p[str[i]-'a'];
+  return pt;        
+}                 
 
- int points(string str){
-    int p[] = {
-      2, // a
-      5, // b
-      4, // c
-      4, // d
-      1, // e
-      6, // f
-      5, // g
-      5, // h
-      1, // i
-      7, // j
-      6, // k
-      3, // l
-      5, // m
-      2, // n
-      3, // o
-      5, // p
-      7, // q
-      2, // r
-      1, // s
-      2, // t
-      4, // u
-      6, // v
-      6, // w
-      7, // x
-      5, // y
-      7, // z      
-    };
-    int pt = 0;
-    for (char c : str)
-      pt+=p[c-'a'];
-    return pt;
-  }
-  
-  Pointify(ifstream &dict){
-    string str = "";
-    dict >> str;
-    while (str != "."){
-      string word = str;
-      sort(str.begin(),str.end());
-      if (H.find(str) == H.end()){
-        H.insert({str,words(points(str))});
-        H[str].L.push_back(word);
+void nck(set<pair<string,string> > &p, string &s, vector<bool> &v, int l, int k){
+  if (count(v.begin(),v.end(),true) == k){
+    string s1 = "";
+    string s2 = "";
+    for (int i = 0; i < v.size(); ++i)
+      if (v[i]) s1+=s[i];
+      else s2+=s[i];
+    if (s1 < s2)
+      p.insert(pair<string,string>(s1,s2));
+    else
+      p.insert(pair<string,string>(s2,s1));
+
+    if (s2.size() > 3){
+      string s3 = "";
+      for (int i = 0; i < s2.size(); ++i){
+        for (int j = 0; j < s2.size(); ++j)
+          if (i != j) s3+=s2[j];
+        if (s1 < s3)
+          p.insert(pair<string,string>(s1,s3));
+        else
+          p.insert(pair<string,string>(s3,s1));
+        s3 = "";
       }
-      else {
-        H[str].L.push_back(word);
-      }
-      
-      dict >> str;
     }
   }
-
-  pair<int,vector<string>> eval(string s) {
-    sort(s.begin(),s.end());
-    vector<string> sol;
-    vector<pair<list<string>*,list<string>*>> w;
-    int maxp = 0;
-    if (H.find(s) != H.end()){
-      maxp = H[s].point;
-      w.push_back({&H[s].L,NULL});
-    }
-    string str1 = "";
-    string str2 = "";
-    for (int i = 0; i < s.size(); ++i){
-      str1+=s[i];
-      for (int j = i+1; j < s.size(); ++j){
-        str1+=s[j];
-        for (int k = j+1; k < s.size(); ++k){
-          str1+=s[k];
-          str2 = "";
-          for (int l = 0; l < s.size(); ++l)
-            if (l != i && l != j && l != k)
-              str2+=s[l];
-          int p = 0;
-          auto h1 = H.find(str1);
-          auto h2 = H.find(str2);
-          if (h1 !=  H.end()){
-            p+=h1->second.point;
-          }
-          if (h2 != H.end()){
-            p+=h2->second.point;
-          }
-          if (p > maxp){
-            w.clear();
-            maxp = p;
-            pair<list<string>*,list<string>*> pair(NULL,NULL);
-            if (h1 != H.end())
-              pair.first = &h1->second.L;
-            if (h2 != H.end())
-              pair.second = &h2->second.L;
-            w.push_back(pair);
-          }
-          if (p == maxp){
-            pair<list<string>*,list<string>*> pair(NULL,NULL);
-            if (h1 != H.end())
-              pair.first = &h1->second.L;
-            if (h2 != H.end())
-              pair.second = &h2->second.L;
-            w.push_back(pair);
-          }
-
-          str1.pop_back();
-        }
-        str1.pop_back();
-      }
-      str1.pop_back();
-    }
-    for (auto p : w){
-      if (p.first != NULL && p.second != NULL){
-        for (auto s1 : *p.first)
-          for (auto s2 : *p.second)
-            if (s1 < s2)
-              sol.push_back(s1 + " " + s2);
-            else
-              sol.push_back(s2 + " " + s1);
-      }
-      else if (p.first != NULL){
-        for (auto s1 : *p.first)
-          sol.push_back(s1);
-      }
-      else if (p.second != NULL){
-        for (auto s1 : *p.second)
-          sol.push_back(s1);
-      }
-      else {}
-    }
-    
-    sort(sol.begin(),sol.end());
-    auto it = unique(sol.begin(),sol.end());
-    sol.resize(distance(sol.begin(),it));
-    return {maxp, sol};
+  for (int i = l+1; i < s.size(); ++i){
+    v[i] = true;
+    nck(p,s,v,i,k);
+    v[i] = false;
   }
-
-
-};
+}
 
 int main(){
-  ifstream dict("lgame.dict");
   ifstream fin("lgame.in");
   ofstream fout("lgame.out");
-  assert(dict.is_open());
+  ifstream dict("lgame.dict");
   assert(fin.is_open());
   assert(fout.is_open());
-  Pointify P(dict);
-  string letters;
-  fin >> letters;
-  pair<int,vector<string>> solution = P.eval(letters);
-  fout << solution.first << endl;
-  for (string s : solution.second){
-    fout << s << endl;
+  assert(dict.is_open());
+  string sw;
+  string w;
+  string str;
+  set<string> solution;
+  int maxp = 0;
+  map<string,vector<string> > M;
+  while(dict>>sw){
+    if (sw == ".") break;
+    w = sw;
+    sort(sw.begin(),sw.end());
+    M[sw].push_back(w);
+  }
+  fin>>str;
+  sort(str.begin(),str.end());
+  set<pair<string,string> > p;
+  vector<bool> v(str.size(),false);
+  for (int i = str.size()/2; i<= str.size(); ++i)
+    nck(p,str,v,-1,i);
+
+  for (set<pair<string,string> >::iterator i = p.begin(); i != p.end(); ++i){
+    int pts1 = 0;
+    int pts2 = 0;
+    if (M.find(i->first) != M.end())
+      pts1+=eval_pts(i->first);
+    if (M.find(i->second) != M.end()){
+      pts2+=eval_pts(i->second);
+    }
+
+    if (pts1+pts2 == 0 || pts1+pts2 < maxp)
+      continue;
+    
+    if (pts1+pts2 > maxp){
+      maxp = pts1+pts2;
+      solution.clear();
+    }
+    
+    if (pts1 && pts2){
+      vector<string> s1 = M[i->first];
+      vector<string> s2 = M[i->second];
+      for (int j = 0; j < s1.size(); ++j)
+        for (int k = 0; k < s2.size(); ++k)
+          if (s1[j] < s2[k])
+            solution.insert(s1[j]+" "+s2[k]);
+          else solution.insert(s2[k]+" "+s1[j]);
+    }
+    else if (pts1){
+      vector<string> s1 = M[i->first];
+      solution.insert(s1.begin(),s1.end());
+    }
+    else {
+      vector<string> s2 = M[i->second];
+      solution.insert(s2.begin(),s2.end());
+    }
   }
 
+  fout << maxp << endl;
+  for (set<string>::iterator i = solution.begin(); i != solution.end(); ++i)
+    fout << *i << endl;
+
+  fin.close();
+  fout.close();
+  dict.close();
   return 0;
 }
